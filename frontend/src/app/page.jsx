8437b80a178321fda1a50c87646838987a79d9de
amankhan2005 <amankhan099k@gmail.com>
2025-03-26 22:40:45 +0530

    new

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -35,7 +35,7 @@ const Home = () => {
       {/* Logo, Social Icons & Sign-In */}
       <div className="bg-white py-4 px-6 flex items-center justify-between">
         {/* Logo Centered */}
-        <div className="flex-1 flex flex-col justify-center">
+        <div className="flex-1 flex  justify-center">
           <span className="text-sky-600 text-3xl font-bold">CYBERAWARE</span>
           <span className="text-gray-600 font-semibold text-sm ml-2">
             Stay Secure, Stay Updated
