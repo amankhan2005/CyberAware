ef8ec9e17b8b7c4d2459a72d6cd077cdb8e6c338
Awaish004 <khanawaish52@gmail.com>
2025-04-27 16:53:53 +0530

    change

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
-import PrelineScript from "@/components/PrelineScript";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <PrelineScript />
         <Toaster />
+        <Navbar />
         {children}
       </body>
     </html>
