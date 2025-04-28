0d93e6688c3d59e26c17960183ff96cc29ffdc5e
Awaish004 <khanawaish52@gmail.com>
2025-04-28 18:15:41 +0530

    nav update

--- frontend/src/app/(main)/layout.jsx
+++ frontend/src/app/(main)/layout.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Layout = ({ children }) => {
     return (
         <>
-            <Navbar />
+            {/* <Navbar />  */}
          
             {children }
         </>
